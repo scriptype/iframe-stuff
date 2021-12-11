f3e08b4fa950df747ef411d6dc97af5fc49c87ba
scriptype <scriptyper@gmail.com>
2021-12-11 17:38:34 +0200

    did it

---
+++ tv.js
@@ -0,0 +1,75 @@
+const penContainer = document.getElementById('penContainer')
+
+const displayDuration = 5 // seconds
+
+const getRandomFrom = (list) => {
+  return list[Math.floor(Math.random() * list.length)]
+}
+
+const getPenDataFromLink = (url) => {
+  const [ , user, slugHash ] = url.match(/(\w+)\/pen\/(\w+)/)
+  return { user, slugHash }
+}
+
+const getElementFromMarkup = (markup) => {
+  const el = document.createElement('div')
+  el.innerHTML = markup
+  return el.firstElementChild
+}
+
+const createIframe = (penData) => {
+  const link = (
+    "https://codepen.io/" +
+    penData.user +
+    "/embed/" +
+    penData.slugHash +
+    "?user=" +
+    penData.user +
+    "&amp;slug-hash=" +
+    penData.slugHash +
+    "&amp;height=100&amp;default-tab=result&amp;name=cp_embed_1"
+  )
+  const html = `
+       <iframe
+           allowfullscreen="true"
+           allowtransparency="true"
+           class="cp_embed_iframe tv"
+           frameborder="0"
+           name="cp_embed_1"
+           scrolling="no"
+           loading="lazy"
+           id="cp_embed_${penData.slugHash}"
+           src="${link}">
+       </iframe>
+  `
+  return getElementFromMarkup(html)
+}
+
+const renderPen = (pen) => {
+  const tv = document.querySelector('.tv')
+  const iframe = createIframe(pen)
+  penContainer.appendChild(iframe)
+  setTimeout(() => {
+    iframe.style.zIndex = 0;
+    if (tv) {
+      tv.remove()
+    }
+  }, tv ? 1500 : 0)
+}
+
+const penData = fetch('./tv.txt')
+  .then(r => r.text())
+  .then(text => text.split('\n'))
+  .then(lines => lines.filter(l => !!l.trim()))
+  .then(links => links.map(getPenDataFromLink))
+
+const init = async () => {
+  const pen = getRandomFrom(await penData)
+  renderPen(pen)
+}
+
+const interval = setInterval(
+  init,
+  displayDuration * 1000
+)
+init()
