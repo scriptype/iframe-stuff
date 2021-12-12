2b90268c97448ceff26c57125a1bc64a6006e3a3
Tarik Akyol <akyoltarik@gmail.com>
2021-12-12 19:40:33 +0000

    Intro to schedule

---
+++ schedule.js
@@ -0,0 +1,38 @@
+const scheduleContainer = document.getElementById('scheduleContainer')
+
+const getNow = scheduleData => {
+  return scheduleData.filter(sD => sD.now == true)[0]
+}
+
+const renderSchedule = (schedule) => {
+  if (!schedule) {
+    return
+  }
+
+  const timeLeft = new Date(schedule.start).getTime() - new Date().getTime()
+  const div = `
+    <div class="overlay-box schedule-box">
+      <div class="schedule-box__title">${schedule.title}</div>
+      <div class="schedule-box__description">${schedule.desc}</div>
+      <div class="schedule-box__url"><a href="${schedule.url}" target="_blank">${schedule.url}</a></div>
+      ${timeLeft > 0 ? `<div class="schedule-box__start">in ${Math.floor(timeLeft / 60000)} minutes</div>` : ''}
+    </div>
+  `
+  scheduleContainer.innerHTML = div
+}
+
+const scheduleData = fetch('./schedule.json')
+  .then(r => r.json())
+  .then(r => r instanceof Array ? r : [])
+
+const scheduleInit = async () => {
+  const now = getNow(await scheduleData)
+  renderSchedule(now)
+}
+
+setInterval(
+  scheduleInit,
+  20000
+)
+
+scheduleInit()
